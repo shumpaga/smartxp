b098779dfe4e3a86415067c2c8bd165e92aeb6f9
Alexandre Gbaguidi Aïsse <alexgbaguidi229@gmail.com>
2021-02-08 16:06:52 -0400

    Organise react project structure

---
+++ src/components/Contact.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+/*
+import PropTypes from 'prop-types';
+import { useForm } from 'react-hook-form';
+import validator from 'validator';
+import ContentHeader from './ContentHeader';
+*/
+
+const Contact = () => {
+  return <Fragment>CONTACT</Fragment>;
+};
+
+export default Contact;
---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+/*
+import PropTypes from 'prop-types';
+import { useForm } from 'react-hook-form';
+import validator from 'validator';
+import ContentHeader from './ContentHeader';
+*/
+
+const Home = () => {
+  return <Fragment>HOME</Fragment>;
+};
+
+export default Home;
